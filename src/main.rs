5508770115a96f3e03274ff3f1daab6d40d0e0a4
Doug Schultz <douglasschultz1@gmail.com>
2024-05-03 16:49:38 -0400

    using layout to create my grid

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+    ExecutableCommand,
+};
+use ratatui::{
+    layout::{Constraint, Direction, Layout, Margin}, prelude::{CrosstermBackend, Stylize, Terminal}, widgets::{Block, Borders, Padding, Paragraph}
+};
+use std::io::{stdout, Result};
+
+fn main() -> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    
+    loop {
+        terminal.draw(|frame| {
+            let verticalLayout = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints(vec![
+                    Constraint::Percentage(20),
+                    Constraint::Percentage(20),
+                    Constraint::Percentage(20),
+                    Constraint::Percentage(20),
+                    Constraint::Percentage(20),
+                ])
+                .split(frame.size());
+
+            for i in 0..verticalLayout.len() {
+                let horizontalLayout = Layout::default()
+                    .direction(Direction::Horizontal)
+                    .constraints(vec![
+                        Constraint::Percentage(20),
+                        Constraint::Percentage(20),
+                        Constraint::Percentage(20),
+                        Constraint::Percentage(20),
+                        Constraint::Percentage(20),
+                    ])
+                    .split(verticalLayout[i]);
+
+                for j in 0..horizontalLayout.len() {
+                    if i == 2 && j == 2 {
+                        frame.render_widget(
+                            Paragraph::new("")
+                                .block(Block::new().borders(Borders::ALL))
+                                .on_white()
+                                .white(),
+                                horizontalLayout[j]);
+                    }
+                }
+            }
+        })?;
+
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press
+                    && key.code == KeyCode::Char('q')
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
